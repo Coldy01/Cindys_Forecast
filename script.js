ed76a9d093760f3495ec471276eb125940c527c3
Cy.1 <cylacson95@gmail.com>
2026-06-24 19:23:53 +0800

    changed back to london

--- script.js
+++ script.js
@@ -96,6 +96,6 @@ function hideError() {
 
 // Load weather for default city on page load
 window.addEventListener('load', () => {
-    cityInput.value = 'Calamba';
+    cityInput.value = 'London';
     handleSearch();
 });
